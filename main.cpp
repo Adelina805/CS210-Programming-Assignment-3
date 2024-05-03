6ba05fe95531fd73d4187bea5bb92b294c06c2ca
Adelina <adelinaballerina1@gmail.com>
2024-05-03 09:27:15 -0700

    working on the fix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <utility>
 using namespace std;
 
@@ -79,6 +78,10 @@ public:
         }
         return *(this->data) == *(otherNode->getData());
     }
+
+    bool getEdgeCost() {
+        return false;
+    }
 };
 
 
@@ -89,13 +92,13 @@ private:
     Node<T> *root;
     int numberOfElements;
     int height;
-    vector<T> heap;
 
 public:
     // Constructor
     BinaryHeap(T *data) {
-        root = new Node<T>(data);
-        numberOfElements = 0;
+        Node<T> *newNode = new Node<T>(data);
+        root = newNode;
+        numberOfElements = 2;
         height = 0;
     }
 
@@ -138,60 +141,94 @@ public:
         delete node;
     }
 
-    void heapifyUp(int index) { // heapify
-        if (index <= 0) return;
-        int parent = (index - 1) / 2;
+    void heapifyUp(Node<T>* node) { // heapify
+        if (node == root) return;
+        Node<T>* parent = node->getParent();
 
-        if (heap[parent].getEdgeCost() > heap[index].getEdgeCost()) {
-
-            swap(heap[parent], heap[index]);
-
+        if (parent->getEdgeCost() > node->getEdgeCost()) {
+            swap(parent, node);
             heapifyUp(parent);
         }
     }
 
-    void heapifyDown(int index) { // heapify
-        int left = 2 * index + 1;
-        int right = 2 * index + 2;
-        int smallest = index;
+    void heapifyDown(Node<T>* node) { // heapify
+        Node<T>* left = node->getLeftChild();
+        Node<T>* right = node->getRightChild();
+        Node<T>* smallest = node;
 
-        if (left < heap.size() && heap[left].getEdgeCost() < heap[smallest].getEdgeCost()) {
+        if (left != nullptr && left->getEdgeCost() < smallest->getEdgeCost()) {
             smallest = left;
         }
 
-        if (right < heap.size() && heap[right].getEdgeCost() < heap[smallest].getEdgeCost()) {
+        if (right != nullptr && right->getEdgeCost() < smallest->getEdgeCost()) {
             smallest = right;
         }
 
-        if (smallest != index) {
-            swap(heap[index], heap[smallest]);
+        if (smallest != node) {
+            swap(node, smallest);
             heapifyDown(smallest);
         }
     }
 
-    void insertElement(T data) {
-        heap.push_back(data);
-        heapifyUp(heap.size() - 1);
+    // insert element recursively
+    void insertElement(T *data) {
+        root = insertElementBH(root, data);
+        numberOfElements++;
+    }
+
+    Node<T> *insertElementBH(Node<T> *node, T *data) {
+        if (node == nullptr) { // if empty, return node
+            return new Node<T>(data);
+        }
+        if (data->getEdgeCost() < node->getData()->getEdgeCost()) { // if smaller than parent, left child
+            node->setLeftChild(insertElementBH(node->getLeftChild(), data));
+        }
+        else if (data->getEdgeCost() > node->getData()->getEdgeCost()) { // if larger than parent, right child
+            node->setRightChild(insertElementBH(node->getRightChild(), data));
+        }
+
+        // After insertion, update the height of the tree
+        height = calculateHeight(root);
+
+        return node;
+    }
+
+    // Function to calculate the height of a node
+    int calculateHeight(Node<T> *node) {
+        if (node == nullptr) {
+            return -1; // Height of an empty tree is -1
+        } else {
+            return 1 + max(calculateHeight(node->getLeftChild()), calculateHeight(node->getRightChild()));
+        }
     }
 
     T deleteMin() {
-        return heap.front();
+        return *root->getData();
+    }
+
+    Node<T> *findLastNode(Node<T> *pNode) {
+        return nullptr;
     }
 
     void remove() {
-        if (heap.empty()) return;
-        heap[0] = heap.back();
-        heap.pop_back();
-        heapifyDown(0);
+        if (root == nullptr) return;
+        // You need to implement a method to find the last node in the heap
+        Node<T>* lastNode = findLastNode(root);
+        root->setData(lastNode->getData());
+        delete lastNode;
+        heapifyDown(root);
     }
 
     bool isEmpty() {
-        return heap.empty();
+        return root == nullptr;
     }
 
     void print() {
-        for (auto& data : heap) {
+        if (root != nullptr) {
+            T data = *root->getData();
             cout << data.getSourceVertex() << " - " << data.getDestinationVertex() << ": " << data.getEdgeCost() << endl;
+        } else {
+            cout << "Heap is empty." << endl;
         }
     }
 };
@@ -253,16 +290,39 @@ public:
 // This method runs the prims algorithm on the graph and prints the output
 void runPrims(int G[VERTEXCOUNT][VERTEXCOUNT], BinaryHeap<Data>* binHeap) {
 
+    // DELETE LATER
+    // Print initial graph and all edge costs
+    cout << "Initial Graph:" << endl;
+    for(int i = 0; i < VERTEXCOUNT; i++) {
+        for(int j = 0; j < VERTEXCOUNT; j++) {
+            cout << "   " << i << " - " << j << ": " << G[i][j] << endl;
+        }
+    }
+    cout << endl;
+
     // initialize visited/unvisited array "0" = false = unvisited and "1" = true = visited
     bool visited[VERTEXCOUNT];
     for (int i = 0; i < VERTEXCOUNT; i++) {
         visited[i] = false; // set all to unvisited
     }
 
+    // DELETE LATER
+    // print visited/unvisited array
+    cout << "updated visited/unvisited: " << endl;
+    for (int i = 0; i < VERTEXCOUNT; i++) {
+        cout << visited[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Prim's MST:" << endl;
+    binHeap->print();
+    cout << endl; // if ALL vertices are visited and "true" the program ends
+
+
     // add all edges adjacent to the start vertex to the priority queue
     for(int i = 0; i < VERTEXCOUNT; ++i) {
         if(G[0][i] != 0) { // if adjacent insert into the binary heap
-            binHeap->insertElement(Data(0, i, G[0][i]));
+            binHeap->insertElement(new Data(0, i, G[0][i]));
         }
     }
 
@@ -286,11 +346,15 @@ void runPrims(int G[VERTEXCOUNT][VERTEXCOUNT], BinaryHeap<Data>* binHeap) {
             // add all edges adjacent to the new vertex to the priority queue
             for (int i = 0; i < VERTEXCOUNT; ++i) {
                 if (G[secondVertex][i] != 0 && !visited[i]) { // if adjacent and unvisited, add to pq and insert into the binary heap
-                    binHeap->insertElement(Data(secondVertex, i, G[secondVertex][i]));
+                    binHeap->insertElement(new Data(secondVertex, i, G[secondVertex][i]));
                 }
             }
         }
+        binHeap->print();
+        cout << "print 1" << endl;
     }
+    binHeap->print();
+    cout << "print 2 " << endl;
 }
 
 
